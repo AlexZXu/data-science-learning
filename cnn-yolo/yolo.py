a504902df0ae78494efbd8c9770f5edb2e45585d
Alex Xu <xualex008@gmail.com>
2026-08-04 15:44:03 -0400

    updates

--- cnn-yolo/yolo.py
+++ cnn-yolo/yolo.py
@@ -32,6 +32,7 @@ class ConvBlock(nn.Module):
     def forward(self, x):
         return self.block(x)
 
+
 class YOLOv1Model(nn.Module):
     def __init__(self):
         super().__init__()
@@ -87,10 +88,11 @@ class YOLOv1Model(nn.Module):
 
         self.prediction = nn.Sequential(
             nn.Flatten(),
-            nn.LazyLinear(4096),
+            nn.LazyLinear(496),
+            nn.Dropout(0.0),
             nn.LeakyReLU(negative_slope=0.1),
-            nn.Linear(4096, 4410)
-        )    
+            nn.Linear(496, 1470)
+        )
 
     def forward(self, x):
         features = self.features(x)
@@ -101,41 +103,44 @@ class YOLOv1Model(nn.Module):
 TRUE LABEL PROCESSING
 """
 
-true_label_file = "cnn-yolo/coco128/labels/train2017/000000000009.txt"
+# true_label_file = "cnn-yolo/coco128/labels/train2017/000000000009.txt"
 
-data = []
-with open(true_label_file, 'r', encoding="utf-8") as file:
-    for line in file:
-        data.append(line.split())
+# data = []
+# with open(true_label_file, 'r', encoding="utf-8") as file:
+#     for line in file:
+#         data.append(line.split())
 
-data = np.array(data, dtype=np.float32)
-x_poses = data[:, 1] * 7
-y_poses = data[:, 2] * 7
+# data = np.array(data, dtype=np.float32)
+# x_poses = data[:, 1] * 7
+# y_poses = data[:, 2] * 7
 
-data[:, 1] = np.floor(x_poses)
-data[:, 2] = np.floor(y_poses)
+# data[:, 1] = np.floor(x_poses)
+# data[:, 2] = np.floor(y_poses)
 
-data = np.insert(data, 3, x_poses - data[:, 1], axis=1)
-data = np.insert(data, 4, y_poses - data[:, 2], axis=1)
+# data = np.insert(data, 3, x_poses - data[:, 1], axis=1)
+# data = np.insert(data, 4, y_poses - data[:, 2], axis=1)
 
-data = data[np.lexsort((data[:, 2], data[:, 1]))]
+# data = data[np.lexsort((data[:, 2], data[:, 1]))]
 
-delete_indexes = []
-for i in range(data.shape[0] - 1):
-    if data[i, 1] == data[i + 1, 1] and data[i, 2] == data[i + 1, 2]:
-        delete_indexes.append(i)
+# delete_indexes = []
+# for i in range(data.shape[0] - 1):
+#     if data[i, 1] == data[i + 1, 1] and data[i, 2] == data[i + 1, 2]:
+#         delete_indexes.append(i)
 
-data = np.delete(data, delete_indexes, axis=0)
+# data = np.delete(data, delete_indexes, axis=0)
 
 """
 TRUE IMAGE PROCESSING
 """
 
-sample_image = Image.open("cnn-yolo/coco128/images/train2017/000000000009.jpg")
-sample_image = sample_image.resize(size=(448, 448))
+# sample_image = Image.open("cnn-yolo/coco128/images/train2017/000000000009.jpg")
+# sample_image = sample_image.resize(size=(448, 448))
 
-image_tensor = torch.Tensor(np.array(sample_image)).permute(2, 0, 1)
-image_tensor = image_tensor.unsqueeze(dim=0)
+# image_tensor = torch.Tensor(np.array(sample_image)).permute(2, 0, 1)
+# image_tensor = image_tensor.unsqueeze(dim=0)
+
+image_tensor = torch.randn((2, 3, 448, 448))
+
 
 """
 MODEL RUNNING
@@ -146,22 +151,23 @@ model = YOLOv1Model()
 optimizer = optim.Adam(model.parameters())
 
 output : torch.Tensor = model(image_tensor)
-output = output.reshape(1, 7, 7, 90)
 
-lambda_coord = 5.0
-lambda_noobj = 0.5
+output = output.reshape(-1, 7, 7, 30)
 
-data_i = 0
+# lambda_coord = 5.0
+# lambda_noobj = 0.5
 
-for i in range(7): #col
-    for j in range(7): #row
-        has_obj_truth = False
-        if (data_i < data.shape[0] and i == data[data_i, 1] and j == data[data_i, 2]):
-            print(i, j)
-            has_obj_truth = True
-            data_i += 1
+# data_i = 0
 
-        output_vec = output[0, i, j]
+# for i in range(7): #col
+#     for j in range(7): #row
+#         has_obj_truth = False
+#         if (data_i < data.shape[0] and i == data[data_i, 1] and j == data[data_i, 2]):
+#             print(i, j)
+#             has_obj_truth = True
+#             data_i += 1
+
+#         output_vec = output[0, i, j]
 
 
 
